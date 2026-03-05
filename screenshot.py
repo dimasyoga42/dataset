8536f1e764707429355caa1eb08d16a9dfd49431
dimasyoga42 <dimasyoga9090@gmail.com>
2026-03-05 22:05:42 +0700

    `refactor: simplify GitHub workflow and update screenshot script`

--- screenshot.py
+++ screenshot.py
@@ -1,7 +1,9 @@
-from playwright.sync_api import sync_playwright
 import time
 
+from playwright.sync_api import sync_playwright
+
 url = "https://tanaka0.work/AIO/en/DyePredictor/ColorWeapon"
+
 
 def auto_scroll(page):
     previous_height = 0
@@ -13,11 +15,17 @@ def auto_scroll(page):
         page.evaluate("window.scrollTo(0, document.body.scrollHeight)")
         time.sleep(1)
 
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=True)
-    page = browser.new_page(viewport={"width":1920,"height":1080})
 
-    page.goto(url, wait_until="networkidle")
+with sync_playwright() as p:
+    browser = p.chromium.launch(
+        headless=True, args=["--no-sandbox", "--disable-dev-shm-usage"]
+    )
+
+    page = browser.new_page(viewport={"width": 1920, "height": 1080})
+
+    page.goto(url, wait_until="domcontentloaded", timeout=120000)
+
+    time.sleep(5)
 
     auto_scroll(page)
 
